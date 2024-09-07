4e69af783964ca280783f75f922d25a832d2a485
benrbowers <benjambowers@gmail.com>
2024-09-07 10:11:51 -0400

    Remove unused function

--- helpers/hexcode.go
+++ helpers/hexcode.go
@@ -5,14 +5,6 @@ import (
 	"image/color"
 )
 
-// func ColorHexToRGB(hexstr string) (r, g, b uint8) {
-// 	r = hexcodeToRGB(hexstr[1:3])
-// 	g = hexcodeToRGB(hexstr[3:5])
-// 	b = hexcodeToRGB(hexstr[5:7])
-
-// 	return r, g, b
-// }
-
 func HexcodeToRGB(hexcode string) color.RGBA {
 	if hexcode[0] == '#' {
 		hexcode = hexcode[1:]
